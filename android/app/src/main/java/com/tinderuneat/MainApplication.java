dd0a580c048c9085931489fb0cc10b94e304965c
mejianestor98 <nestor.mejia@alumnos.uneatlantico.es>
2019-05-19 18:26:14 +0200

    Deck working with both swipe and buttons

--- android/app/src/main/java/com/tinderuneat/MainApplication.java
+++ android/app/src/main/java/com/tinderuneat/MainApplication.java
@@ -3,6 +3,8 @@ package com.tinderuneat;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.entria.views.RNViewOverflowPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNViewOverflowPackage(),
+            new RNGestureHandlerPackage(),
             new RNGoogleSigninPackage()
       );
     }
