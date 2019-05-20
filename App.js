2fdaf36a2b2e86f74707a88e678619c687756c0d
VictorMaderaA <victor.madera.a@gmail.com>
2019-05-20 08:28:45 +0200

    Testapk

--- App.js
+++ App.js
@@ -1,138 +1,32 @@
-import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, ImageBackground } from 'react-native';
-import {Router, Stack, Scene} from 'react-native-router-flux';
-import WelcomeScreen from './screens/WelcomeScreen';
-import LoginScreen from './screens/LoginScreen';
-import StackScreen from './screens/StackScreen';
-import CounterScreen from './screens/CounterScreen';
-import MainScreen from './screens/MainScreen';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
 
 type Props = {};
 export default class App extends Component<Props> {
-
-  state = {
-    currWindow: 'splash',
-    gender: null,
-  };
-
-  render() {    
-
-    // return (
-    //   <Router showNavigationBar={true}>
-    //     <Stack key="root">
-    //       <Scene key="login" component={LoginScreen} hideNavBar title="Login"  />
-    //       <Scene key="welcome" component={WelcomeScreen} hideNavBar title="Register"  />
-    //       <Scene key="stack" component={StackScreen} hideNavBar title="Stack" initial/>
-    //     </Stack>
-    //   </Router>
-    // );
-    console.log(this.state.currWindow)
-
-    if (this.state.currWindow == 'splash')  //Splash
-    {
-      this.splashScreenTimer();
-      return (
-        <View>
-          <ImageBackground source={require('./assets/images/splashscreen.png')} style={{width: '100%', height: '100%'}}/>
-        </View>
-      );
-    } 
-    else if (this.state.currWindow == 'login') //Login 
-    {
-      return (
-        <View>
-          <LoginScreen parent={this} style={{width: '100%', height: '100%'}}/>
-        </View>
-      );
-    }
-    else if (this.state.currWindow == 'gender') //Gender Select
-    {
-      return (
-        <WelcomeScreen parent={this}/>
-      );
-    }
-    else if (this.state.currWindow == 'timer')
-    {
-      return (
-        <CounterScreen parent={this}/>
-      );
-    }    
-    else if (this.state.currWindow == 'main')//MainPage
-    {
-      return (
-        <MainScreen parent={this} gender={this.state.gender}/>
-      );
-    }
-    else 
-    {
-      return (
-        <Router showNavigationBar={true}>
-          <Stack key="root">
-            <Scene key="login" component={LoginScreen} hideNavBar title="Login"  />
-            <Scene key="welcome" component={WelcomeScreen} hideNavBar title="Register"  />
-            <Scene key="stack" component={StackScreen} hideNavBar title="Stack" initial/>
-          </Stack>
-        </Router>
-      );
-    }
-    
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
+    );
   }
-
-  splashScreenTimer = async () => {
-    var now = new Date();
-    now.setMonth(5);
-    now = now.getTime();
-    var selectedDate = (new Date(2019, 5, 20, 15).getTime() / 1);
-
-    this.dateValues(selectedDate);
-    this.dateValues(now);
-    
-
-    console.log(selectedDate + ' < ' + now + ' ? = '+ (selectedDate<now));
-    if (selectedDate < now) {
-      console.log("Selected date is in the past");
-      setTimeout(function(){this.viewChangeLogin()}.bind(this), 3000);
-    } else {
-      console.log("Selected date is NOT in the past");
-      setTimeout(function(){this.viewChangeTimer()}.bind(this), 3000);
-    }
-  }
-
-  dateValues = (date) => {
-
-    var date = new Date(date);
-    var month = date.getMonth();
-    var day = date.getDay();
-    var hour = date.getHours();
-    var minute = date.getMinutes();
-    var second = date.getSeconds();
-
-    console.log(month + '-' + day + ' ' + hour + ':' + minute + ':' + second);
-
-  }
-
-
-  viewChangeLogin = () => {
-    this.setState({ currWindow: 'login' })
-  }
-
-  viewChangeGender = () => {
-    this.setState({ currWindow: 'gender' })
-  }
-
-  viewChangeSplash = () => {
-    this.setState({ currWindow: 'splash' })
-  }
-
-  viewChangeTimer = () => {
-    this.setState({ currWindow: 'timer' })
-  }
-
-  viewChangeMainScreen = () => {
-    this.setState({ currWindow: 'main' })
-  }
-
-
 }
 
 const styles = StyleSheet.create({
